63195b3ba892cc9ae810c9d2f7f6ad1a595c500f
Robert Zupke <rdzupke@me.com>
2021-03-16 14:38:00 -0700

    Added another test image, test-3.png, and updated the test shell script to run from the root of the repo

--- test_images/test.sh
+++ test_images/test.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/sh
 
-../ssocr -T -S -d -1 test-1.png
-../ssocr -T -S -d -1 test-2.png
+./ssocr -T -S -d -1 $(find .  -name test-1.png)
+./ssocr -T -S -d -1 $(find .  -name test-2.png)
+./ssocr -T -S -d -1 $(find .  -name test-3.png)
