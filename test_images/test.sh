2467776cef0c3ec548681f1b25f79c5656504d2d
Robert Zupke <rdzupke@me.com>
2021-04-07 19:03:53 -0700

    Added test scripting for test dyson images

--- test_images/test.sh
+++ test_images/test.sh
@@ -19,3 +19,38 @@ echo "Testing test-3.png"
 ./ssocr -T -S -d -1 $(find .  -name test-3.png)
 echo "Expected:"
 echo "--88Uu-_8\n"
+
+echo "========================="
+./ssocr crop 1808 1480 450 310 -t 25 -D -S -a --background=black --foreground=white -d -1 $(find .  -name dyson-hc-le-02.png)
+echo "Expected:"
+echo "02\n"
+
+echo "========================="
+./ssocr crop 1808 1480 450 310 -t 25 -D -S -a --background=black --foreground=white -d -1 $(find .  -name dyson-hc-le-6h.png)
+echo "Expected:"
+echo "6h\n"
+
+echo "========================="
+./ssocr crop 1808 1480 450 310 -t 25 -D -S -a --background=black --foreground=white -d -1 $(find .  -name dyson-hc-le-74.png)
+echo "Expected:"
+echo "74\n"
+
+echo "========================="
+./ssocr crop 1808 1480 450 310 -t 25 -D -S -a --background=black --foreground=white -d -1 $(find .  -name dyson-hc-le-narrow.png)
+echo "Expected:"
+echo "11\n"
+
+echo "========================="
+./ssocr crop 1808 1480 450 310 -t 25 -D -S -a --background=black --foreground=white -d -1 $(find .  -name dyson-hc-le-off.png)
+echo "Expected:"
+echo "  \n"
+
+echo "========================="
+./ssocr crop 1808 1480 450 310 -t 25 -D -S -a --background=black --foreground=white -d -1 $(find .  -name dyson-hc-le-sleep-off.png)
+echo "Expected:"
+echo "--\n"
+
+echo "========================="
+./ssocr crop 1808 1480 450 310 -t 25 -D -S -a --background=black --foreground=white -d -1 $(find .  -name dyson-hc-le-wide.png)
+echo "Expected:"
+echo "??\n"
