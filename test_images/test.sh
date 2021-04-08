2cd38ede64c8c2b0ffdd03aea6124e2022eb6a72
Robert Zupke <rdzupke@me.com>
2021-04-07 18:49:37 -0700

    Updated the test script to be more verbose and organized

--- test_images/test.sh
+++ test_images/test.sh
@@ -1,5 +1,21 @@
 #!/usr/bin/sh
 
+echo "\n=========================\nStarting test cases...\n=========================\n"
+
+echo "========================="
+echo "Testing test-1.png"
 ./ssocr -T -S -d -1 $(find .  -name test-1.png)
+echo "Expected:"
+echo "8Hh??Uun-\n"
+
+echo "========================="
+echo "Testing test-2.png"
 ./ssocr -T -S -d -1 $(find .  -name test-2.png)
+echo "Expected:"
+echo "?6btr8888\n"
+
+echo "========================="
+echo "Testing test-3.png"
 ./ssocr -T -S -d -1 $(find .  -name test-3.png)
+echo "Expected:"
+echo "--88Uu-_8\n"
